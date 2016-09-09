c09356487360ec373c98ec50800a450a3966a60f
bFraley <bFraley@users.noreply.github.com>
2016-09-09 13:46:39 -0400

    define prompt function

---
+++ lib.py
@@ -0,0 +1,11 @@
+# weather-app
+# lib.py
+# Classes and functions for weather-app.
+
+
+# Function definitions.
+# ---------------------
+
+# Prompt for user input. Accepts a prompt message we want to show.
+def prompt(msg):
+    return input(msg)
